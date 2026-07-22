2478ffa7e187d423f842ffe6a006ac3a8fc0c63d
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-07-22 22:45:33 +0530

    unproductive day

--- Remove_dupplicates_from_sorted_array.py
+++ Remove_dupplicates_from_sorted_array.py
@@ -36,4 +36,23 @@ def remove_duplicates_optimal(arr):
     return i+1
 arr = [1,1,1,2,3,4,4,7,9,9,9,10,22,3,2,2,1,7,23]
 print(remove_duplicates_optimal(arr))
+
+
+
+
+def remove_duplicates_optimal(arr):
+    n = len(arr)
+    if n == 1 :
+        return 1 
+    i = 0
+    j = i+1
+    while j<n:
+        if arr[j] != arr[i]:
+            
+            arr[i],arr[j] = arr[j],arr[i]
+            i = i+1
+        j = j+1
+    return i+1
+arr = [1,1,1,2,3,4,4,7,9,9,9,10,22,3,2,2,1,7,23,57,77,39,69,92,97]
+print(remove_duplicates_optimal(arr))
         
